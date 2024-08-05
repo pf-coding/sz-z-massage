7fe19e57a1f3585956119b08c386ee4a29948461
pf-coding <peterfarkas.coding@gmail.com>
2024-08-05 16:15:01 +0200

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,20 @@
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBDbv6ZmHKK9-eA6EtsLxRvMd--iHHHiMs',
-    authDomain: 'sz-z-relaxz.firebaseapp.com',
-    projectId: 'sz-z-relaxz',
-    storageBucket: 'sz-z-relaxz.appspot.com',
-    messagingSenderId: '987823927980',
-    appId: '1:987823927980:web:55ae81197e31a7bb672509',
-  },
+    apiKey: process.env['FIREBASE_API_KEY'] || '',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['FIREBASE_APP_ID'] || '',
+  } as FirebaseConfig,
 };
