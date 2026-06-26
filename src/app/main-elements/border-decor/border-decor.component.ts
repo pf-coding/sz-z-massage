1ea9747cbc8f641c9e897cc5c0a552c64225b681
Péter Farkas <peterfarkas.coding@gmail.com>
2026-06-26 12:23:45 +0200

    prefixes

---
+++ src/app/main-elements/border-decor/border-decor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-border-decor',
+  templateUrl: './border-decor.component.html',
+  styleUrls: ['./border-decor.component.scss']
+})
+export class BorderDecorComponent {
+
+}
